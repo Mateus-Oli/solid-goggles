6e522ca4d1a0f5ab6ca09ebd8a21c47c235607a8
Mateus <mateus.oli.car@gmail.com>
2018-01-01 04:37:05 -0200

    fix  default factory order

--- src/providers/defaultFactory.js
+++ src/providers/defaultFactory.js
@@ -2,15 +2,15 @@ export function defaultFactory(implementation, args = []) {
   return getFactory(implementation)(implementation, args);
 }
 
-const getFactory = value => (factories.find(validator => validator(value)) || basic)[1];
-
-const factories = [
-  [isConstructor, (value, args) => new value(...args)],
-  [isLambda, (value, args) => value(...args)]
-];
-const basic = [, value => value];
+const getFactory = value => (factories.find(([ validator ]) => validator(value)) || basic )[1];
 
 const isLambda = value => isFunction(value) && !value.prototype;
 const isConstructor = value => isFunction(value) && value.prototype;
 
 const isFunction = value => typeof value === 'function';
+
+const factories = [
+  [isLambda, (value, args) => value(...args)],
+  [isConstructor, (value, args) => new value(...args)]
+];
+const basic = [, value => value];
