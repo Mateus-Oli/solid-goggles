377b3e042e0b682e42b112c51912055a30c4d366
Mateus <mateus.oli.car@gmail.com>
2018-01-21 02:00:29 -0200

    install babel-jest

--- test/utils/findReturn.spec.js
+++ test/utils/findReturn.spec.js
@@ -3,5 +3,5 @@ import { findReturn } from "../../src/utils/findReturn";
 describe('find return', () => {
   it('executes with right parameters', () => findReturn([10], v => expect(v).toBe(10)));
   it('returns first returned truthy value', () => expect(findReturn([0, 0, 0, 1, 0], v => v)).toBe(1));
-  it('returns undefined without truthy value', expect(findReturn([1, 2, 3, 4, 5], () => false)).toBe(undefined));
+  it('returns undefined without truthy value', () => expect(findReturn([1, 2, 3, 4, 5], () => false)).toBe(undefined));
 });
