4a19500e050e4d13aea271c6b7a84cb07e2d3c30
Mateus <mateus.oli.car@gmail.com>
2018-01-28 19:43:59 -0200

    fix parameter decorator check

--- src/decorators/connect.js
+++ src/decorators/connect.js
@@ -8,6 +8,6 @@ const makeConnect = (hook, returnV) => (target, property, T) => {
 const connectParameter = makeConnect(parameters, []);
 const connectProperty = makeConnect(properties, {});
 
-export const connect = T => (target, property, length) => property ?
-  connectProperty(target, property, T) :
-  connectParameter(target, length, T);
+export const connect = T => (target, property, length) => typeof length === 'number' ?
+  connectParameter(target, length, T) :
+  connectProperty(target, property, T);
